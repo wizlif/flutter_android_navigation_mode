4964473dfb3927f239c29f1649ccc46a048bfb6f
rodgh522 <rodgh522@gmail.com>
2025-07-29 10:31:57 +0900

    handle old android version

--- android/src/main/kotlin/com/flutter_android_navigation_mode/FlutterAndroidNavigationModePlugin.kt
+++ android/src/main/kotlin/com/flutter_android_navigation_mode/FlutterAndroidNavigationModePlugin.kt
@@ -1,8 +1,11 @@
 package com.flutter_android_navigation_mode
 
+import android.view.ViewConfiguration
 import android.content.Context
 import android.content.res.Resources
 import androidx.annotation.NonNull
+import android.provider.Settings
+import android.os.Build
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
@@ -25,16 +28,93 @@ class FlutterAndroidNavigationModePlugin: FlutterPlugin, MethodCallHandler {
   }
 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "get_navigation_mode") {
-      val resourceIdentity = context.resources.getIdentifier("config_navBarInteractionMode", "integer", "android")
-      val navMode = context.resources.getInteger(resourceIdentity)
-      result.success(navMode)
-    } else {
-      result.notImplemented()
+    when (call.method) {
+      "get_navigation_mode" -> {
+        val navigationMode = when {
+          Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q -> {
+            getNavigationMode()
+          }
+
+          Build.VERSION.SDK_INT >= Build.VERSION_CODES.P -> {
+            getNavigationModeForPie()
+          }
+
+          else -> {
+            getLegacyNavigationMode()
+          }
+        }
+
+        result.success(navigationMode)
+      }
+      else -> result.notImplemented()
     }
   }
 
   override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
     channel.setMethodCallHandler(null)
+  }
+
+  private fun getNavigationMode(): Int {
+    return try {
+      val resources = context.resources
+      val resourceId = resources.getIdentifier(
+        "config_navBarInteractionMode",
+        "integer",
+        "android"
+      )
+      if (resourceId != 0) {
+        resources.getInteger(resourceId)
+      } else {
+        Settings.Secure.getInt(
+          context.contentResolver,
+          "navigation_mode",
+          0
+        )
+      }
+    } catch (e: Exception) {
+      0 // default: 3-button navigation
+    }
+  }
+
+  // Android 9
+  private fun getNavigationModeForPie(): Int {
+    return try {
+      Settings.Secure.getInt(
+        context.contentResolver,
+        "navigation_mode",
+        0
+      )
+    } catch (e: Exception) {
+      0
+    }
+  }
+
+  // Android legacy
+  private fun getLegacyNavigationMode(): Int {
+    return try {
+      if (hasNavigationBar()) {
+        0 // 3-button navigation
+      } else {
+        2 // Gesture navigation
+      }
+    } catch (e: Exception) {
+      0 // default: 3-button navigation
+    }
+  }
+
+  private fun hasNavigationBar(): Boolean {
+    val resources = context.resources
+    val resourceId = resources.getIdentifier("config_showNavigationBar", "bool", "android")
+    return if (resourceId > 0) {
+      resources.getBoolean(resourceId)
+    } else {
+      ViewConfiguration.get(context).hasPermanentMenuKey().not()
+    }
+  }
+
+  companion object {
+    const val NAVIGATION_MODE_THREE_BUTTONS = 0
+    const val NAVIGATION_MODE_TWO_BUTTONS = 1
+    const val NAVIGATION_MODE_GESTURES = 2
   }
 }
